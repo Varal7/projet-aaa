813b941245397285c05aa0e9968cf59e65b3e90f
Louis Rémus <louisremus@gmail.com>
2016-10-17 19:57:55 +0200

    Organisation et normalisation

---
+++ main.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from euclideanDistance import euclideanDistance
+from loadData import *
+# X contient notre data 7 stats
+# names contient les noms des joueurs et leur équipe
+# column contient les noms des colonnes
+X, names, column = loadData('data_players_7stats.csv')
+
+# Plotting data
+# plotData(X,column)
+
+# Normalisation
+X = (X - np.mean(X, axis = 0)) / np.std(X, axis = 0)
