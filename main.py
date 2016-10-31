7ae65824d7801d8b6580d407d276cb6cec354097
Louis Rémus <louisremus@gmail.com>
2016-10-31 18:28:47 +0100

    Premier clustering avec MeanShift

--- main.py
+++ main.py
@@ -7,17 +7,27 @@ import matplotlib.pyplot as plt
 from euclideanDistance import euclideanDistance
 from loadData import *
 from isomap import *
+from affinityPropagation import *
+from meanShift import *
 
-# X contient notre data 7 stats
+# X contient notre dataset 7 stats
 # names contient les noms des joueurs et leur équipe
 # column contient les noms des colonnes
 X, names, column = loadData('data/data_players_7stats.csv')
 
-# Plotting data
+computeMeanShift(X)
+
+# Compute Mahalanobis distance
+sp.spatial.distance.mahalanobis(X[0], X[1], np.linalg.inv(np.cov(np.transpose(X))))
+
+# N'a pas de sens pour l'instant
+# plotMeanShift(X)
+
+# Plotting data and data distribution
 # plotData(X, column)
 
 # Normalisation
-X = (X - np.mean(X, axis = 0)) / np.std(X, axis = 0)
+# X = (X - np.mean(X, axis = 0)) / np.std(X, axis = 0)
 
 # ploting Isomap
-isomap(X, 3, True)
+# isomap(X, 3, True)
