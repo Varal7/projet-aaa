78a021390797fd49283e1205281a8b9ff0b7e7a5
Marc Thibault <marc.thibault@jalgos.com>
2017-01-19 10:49:33 +0100

    studying the angle feature

---
+++ anglestudy.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+import matplotlib.pyplot as plt
+from xgboost import plot_tree
+from sklearn import decomposition
+
+from utilitiesData import *
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+
+def fengineering(X):
+    # X = (X - np.mean(X, axis = 0)) / np.std(X, axis = 0)
+    # tan()
+    # pca = PCA(n_components = 1)
+    # first_component = pca.fit_transform(X)
+    # X.append()
+    return X
+
+
+def error_predict_origin(X0 = np.array([0, 0])):
+    X = pd.read_csv('data/data_2010-2011.csv', sep = ',')
+    df = pd.DataFrame(data = X)
+
+    d = dict(zip(df['GenPosition'].unique(), range(df['GenPosition'].unique().size)))
+    df['GenPosition'] = df['GenPosition'].apply(lambda x:d[x])
+
+    predictors = ['Rebounds', 'Assists', 'Turnovers', 'Steals', 'Blocks', 'PersonalFouls', 'Points']
+
+    pca = decomposition.PCA(2)
+    pca.fit(df[predictors])
+    projection = np.dot(df[predictors], pca.components_.T)
+    projection -= X0
+    angle = np.zeros((projection.shape[0], 1))
+    angle[projection[:, 1] == 0, 0] = np.pi / 2
+    angle[projection[:, 1] != 0, 0] = np.arctan(projection[projection[:, 1] != 0][:, 0] / projection[projection[:, 1] != 0][:, 1])
+
+
+    score = score_prediction(angle, df['GenPosition'])
+    return score
+
+
+def score_prediction(X, Y):
+    clf = xgb.XGBClassifier(n_estimators = 100,
+                                max_depth = 5,
+                                objective = 'multi:softmax',
+                                subsample = 0.5)
+    clf.fit(X, Y, eval_set = [(X, Y)], verbose = False)
+
+    evals_result = clf.evals_result()
+
+    score = evals_result["validation_0"]["merror"][-1]
+    return score
+
+
+def optimize_origin(grid, xmin, xmax, ymin, ymax):
+    scores = np.zeros([grid, grid])
+    for i in range(grid):
+        for j in range(grid):
+            origin = np.array([xmin + (xmax - xmin) * i / grid, ymin + (ymax - ymin) * j / grid])
+            scores[i, j] = error_predict_origin(origin)
+
+    print(scores)
+    plt.imshow(scores, cmap='hot', interpolation='nearest')
+    plt.show()
+
+    # return scores
+
+
+def main():
+    optimize_origin(30, -5, 5, -5, 5)
+
+if  __name__ =='__main__':main()
+
+    #
